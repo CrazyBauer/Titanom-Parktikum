a5b1370ed85c05b108d1893b02d8a738ba4e73a0
CrazyBauer <delongeleon@gmail.com>
2023-03-30 11:27:26 +0200

    Update ki.js

--- KItest/ki.js
+++ KItest/ki.js
@@ -1,3 +1,14 @@
 const text = "My dog ate my";
 const model = "davinci";
 const apiKey = "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";
+const url = "https://api.openai.com/v1/completions";
+const header = {
+  "Content-Type": "application/jason",
+  Authorization: `Bearer ${apiKey}`,
+};
+
+const requestBody = {
+  text,
+  model,
+  n: 1,
+};
