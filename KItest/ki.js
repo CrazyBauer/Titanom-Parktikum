c4c2cfc61f1853d2156acd0bd5a3f3ea25f2c19d
CrazyBauer <delongeleon@gmail.com>
2023-03-30 12:19:55 +0200

    a

--- KItest/ki.js
+++ KItest/ki.js
@@ -1,14 +1,25 @@
-const text = "My dog ate my";
+const prompt = "My dog ate my";
 const model = "davinci";
-const apiKey = "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";
-const url = "https://api.openai.com/v1/completions";
+const apiKey = "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";
+const apiUrl = "https://api.openai.com/v1/completions";
 const header = {
-  "Content-Type": "application/jason",
+  "Content-Type": "application/json",
   Authorization: `Bearer ${apiKey}`,
 };
 
 const requestBody = {
-  text,
-  model,
+  prompt: prompt,
+  model: model,
   n: 1,
 };
+
+fetch(apiUrl, {
+  method: "POST",
+  headers: header,
+  body: JSON.stringify(requestBody),
+})
+  .then((Response) => Response.json())
+  .then((data) => {
+    console.log(data.choices[0].text);
+  })
+  .catch((error) => console.log(error));
