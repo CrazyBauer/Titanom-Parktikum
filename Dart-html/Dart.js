a2397364876bf2956a96b5cb426664bb8ad07e51
CrazyBauer <delongeleon@gmail.com>
2023-03-30 15:15:14 +0200

    Großes Update

--- Dart-html/Dart.js
+++ Dart-html/Dart.js
@@ -1,5 +1,5 @@
-let SpielstandS1 = 501;
-let SpielstandS2 = 501;
+let Spieler1score = 501;
+let Spieler2score = 501;
 const punkteEingabefeld = document.getElementById("Punkteingabefeld");
 const Spieler1 = document.getElementById("Spieler1");
 const Spieler2 = document.getElementById("Spieler2");
@@ -7,8 +7,10 @@ const GewonneneLegsS1 = document.getElementById("bishergewonnenS1");
 const GewonneneLegsS2 = document.getElementById("bishergewonnenS2");
 const GewonneneSetsS1 = document.getElementById("bishergewonnenS1");
 const GewonneneSetsS2 = document.getElementById("bishergewonnenS2");
-document.getElementById("SpielstandtSpieler1").innerHTML = SpielstandS1;
-document.getElementById("SpielstandtSpieler2").innerHTML = SpielstandS2;
+document.getElementById("SpielstandtSpieler1").innerHTML = Spieler1score;
+document.getElementById("SpielstandtSpieler2").innerHTML = Spieler2score;
+const Spieler1Active = Spieler1.active;
+const Spieler2Active = Spieler2.active;
 
 document.getElementById("button1");
 document.getElementById("Punkteingabefeld");
@@ -21,48 +23,40 @@ function addvalue(value) {
   } else {
     punkteEingabefeld.classList.add("red");
   }
-  //console.log(punkteEingabefeld.value, punkteEingabefeld.classList);
-}
-
-Spieler2 = Objekt();
-{
-  Spieler1.aktiv = true;
-  Spieler1.name = "Spieler1";
-  Spieler1.Spielstad = SpielstandS1;
-}
-Spieler2 = Objekt();
-{
-  Spieler2.aktiv = false;
-  Spieler2.name = "Spieler2";
-  Spieler2.Spielstad = SpielstandS2;
-}
-
-function neueraktiverSpieler() {
-  if (Spieler1.aktiv) {
-    !Spieler1.aktiv;
-    !Spieler2.aktiv;
-  } else {
-    !Spieler1.aktiv;
-    !Spieler2.aktiv;
-  }
 }
 function updateSpielstand() {
-  if (Spieler1.classList.contains("aktiverSpieler")) {
-    SpielstandS1 = SpielstandS1 - punkteEingabefeld.value;
+  if (Spieler1.activ) {
+    Spieler1.SpielstandS1 = Spieler1.SpielstandS1 - punkteEingabefeld.value;
   } else {
-    SpielstandS2 = SpielstandS2 - punkteEingabefeld.value;
+    Spieler2.SpielstandS2 = Spieler2.SpielstandS2 - punkteEingabefeld.value;
   }
 }
 
 function Spielzugabschließen() {
-  updateSpielstand();
-  neueraktiverSpieler();
+  scoreupdate();
+  updateactivSpieler();
+  return punkteEingabefeld((value = ""));
 }
 
-//function spieler1startet() {
-//Spieler1.classList.add("aktiverSpieler");
-//  Spieler2.classList.remove("aktiverSpieler");
-//}
-//function spieler2startet() {
-//  Spieler2.classList.add("aktiverSpieler");
-//  Spieler1.classList.remove("aktiverSpieler");}
+function spieler1startet() {
+  Spieler1.active = true;
+  Spieler2.active = false;
+}
+function spieler2startet() {
+  Spieler1.active = false;
+  Spieler2.active = true;
+}
+
+function scoreupdate(Spieler1, Spieler2) {
+  if (Spieler1.active) {
+    Spieler1score = Spieler1score - punkteEingabefeld.value;
+  } else {
+    Spieler2score = Spieler2score - punkteEingabefeld.value;
+  }
+}
+function updateactivSpieler() {
+  if (Spieler1.classList.contains("SpielerActive")) {
+  }
+  Spieler1active = !Spieler1.active;
+  Spieler2active = !Spieler2.active;
+}
